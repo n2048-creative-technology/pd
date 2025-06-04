7ff8a07f04277392bf4eee23c1f964a83e86e9f6
Mauricio van der Maesen de Sombreff <mauricio@n2048.com>
2025-06-04 15:29:27 +0200

    add ENTTEC integration

--- dmx/blackout.py
+++ dmx/blackout.py
@@ -3,5 +3,6 @@ from dmx import send_batch_dmx
 from random import random
 
 port = '/dev/ttyUSB0'
-send_batch_dmx(0,port)    # On
+
+send_batch_dmx(0,port)    # Off
         
--- dmx/full_on.py
+++ dmx/full_on.py
@@ -4,5 +4,4 @@ from random import random
 
 port = '/dev/ttyUSB0'
 
-for ch in range(12):
-    send_batch_dmx(255,port)    # On
+send_batch_dmx(255,port)    # On
